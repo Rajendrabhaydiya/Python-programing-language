94ec77782f14ac49516dbd40322f0863b899a893
Rajendra Bhaydiya <143413718+Rajendrabhaydiya@users.noreply.github.com>
2023-11-12 23:06:00 +0530

    Create Maximize It!.py

---
+++ Maximize It!.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import product
+
+K,M = map(int,input().split())
+N = (list(map(int, input().split()))[1:] for _ in range(K))
+results = map(lambda x: sum(i**2 for i in x)%M, product(*N))
+print(max(results))
