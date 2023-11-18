51fe6946e040efb5ac06d4298535e410a77971ab
Rajendra Bhaydiya <143413718+Rajendrabhaydiya@users.noreply.github.com>
2023-11-18 18:22:10 +0530

    Create The Minion Game.py

---
+++ The Minion Game.py
@@ -0,0 +1,16 @@
+def minion_game(string):
+    # your code goes here
+    vowel = 'aeiou'.upper()
+    strl = len(string)
+    kevin = sum(strl-i for i in range(strl) if string[i] in vowel)
+    stuart = strl*(strl + 1)/2 - kevin
+
+    if kevin == stuart:
+        print ('Draw')
+    elif kevin > stuart:
+        print ('Kevin %d' % kevin)
+    else:
+        print ('Stuart %d' % stuart)
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
