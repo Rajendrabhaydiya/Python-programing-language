7cd7b2cc7161ea3b1c0359b4e58ccbe681923b11
Rajendra Bhaydiya <143413718+Rajendrabhaydiya@users.noreply.github.com>
2023-11-18 18:29:44 +0530

    Create No Idea!.py

---
+++ No Idea!.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n, m = input().split()
+
+sc_ar = input().split()
+
+A = set(input().split())
+B = set(input().split())
+print(sum([(i in A) - (i in B) for i in sc_ar]))
